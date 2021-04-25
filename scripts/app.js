11dec414cc9a1b3924c82726214caef474a59eef
Jonathan Doorn <jdoorn@purdue.edu>
2021-04-25 11:42:43 -0400

    pagination working with two buttons now

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,50 @@ function getAllFeed(user_hash, callback) {
     });
 }
 
+Vue.component('pagination',
+    {
+        props: ['pdata'],
+        data: function () {
+            return {
+                pages: Math.ceil(this.pdata.length / 5),
+                currentPage: 1,
+                currentSlice: this.pdata.slice(0, 5)
+            }
+        },
+        methods: {
+            goToPage: function (pnum) {
+                let idx = pnum - 1;
+                this.currentPage = pnum;
+                this.currentSlice = this.pdata.slice(idx*5, idx*5 + 5);
+            },
+            nextpage:function(){
+                if (this.currentPage < this.pages) {
+                    this.currentPage += 1;
+                    this.goToPage(this.currentPage);
+                }
+            },
+            prevpage: function (){
+                if (this.currentPage > 1) {
+                    this.currentPage -=1;
+                    this.goToPage(this.currentPage);
+                }
+            }
+        },
+        template: `
+          <div class="pagination">
+            <div class="container">
+              <post-item v-for="p in currentSlice"
+                         v-bind:key="p.id"
+                         v-bind:post="p"></post-item>
+            <button v-on:click="prevpage">Prev page</button>
+            <button v-on:click="nextpage">Next page</button>
+            </div>
+            
+          </div>
+        `
+    }
+    );
+
 Vue.component('post-item',
     {
         props:['post'],
@@ -50,9 +94,82 @@ const app = new Vue( {
     methods: {}
     ,
     created: function () {
-        fetch('/api/'+USERHASH+"/feed")
-            .then(response => response.json())
-            .then(data => (this.post = data));
+        // fetch('/api/'+USERHASH+"/feed")
+        //     .then(response => response.json())
+        //     .then(data => (this.post = data));
+        this.post = [{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },{
+            handle: "technojd",
+            post_content: "Hurray this is my first post",
+            post_creator: "technojd",
+            post_id: 1,
+            post_timestamp: "2021-04-25 00:06:11",
+            post_title: "My first post",
+            post_votes: 2,
+            time_passed: "Approximately 930 Minutes Ago"
+        },]
     }
 
 })
